24156ca8e1e469cac323b1bc221bd47af9653774
Bilal MB <bmbenkada@gmail.com>
2024-04-15 18:52:52 +0100

    Added data annotations to models to validate data. Will consider FluentValidation

--- Models/PointOfInterestForCreationDTO.cs
+++ Models/PointOfInterestForCreationDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CityinfoAPI.Models
 {
     public class PointOfInterestForCreationDTO
     {
+        [Required(ErrorMessage = "Please provide a name.")] // Everything has a name!
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
