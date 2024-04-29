63be33ab3f576f47bd3c2cee69a960cb498e85b3
Bilal MB <bmbenkada@gmail.com>
2024-04-29 15:09:57 +0100

    Fixed issue with missing implementation in LocalMailService

--- Services/LocalMailService.cs
+++ Services/LocalMailService.cs
@@ -1,17 +1,19 @@
+using CityInfoAPI.Services;
+
 namespace CityinfoAPI.Services
 {
-    public class LocalMailService
+    public class LocalMailService : IMailService
     {
         private string _mailTo = "admin@mycompany.com";
         private string _mailFrom = "noreply@mycompany.com";
 
 
-    public void Send(string subject, string message)
-    {
-        //Send mail- Outputs to console
-        Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(LocalMailService)}");
-        Console.WriteLine($"Subject: {subject}");
-        Console.WriteLine($"Message: {message}");
-    }
+        public void Send(string subject, string message)
+        {
+            //Send mail- Outputs to console
+            Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(LocalMailService)}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+        }
     }
 }
